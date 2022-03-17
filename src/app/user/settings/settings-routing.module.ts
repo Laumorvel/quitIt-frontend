2c59cdc4cfb2d4c68f5f033571655108f56e814c
adelalira <aalira.96@gmail.com>
2022-03-17 10:24:03 +0100

    rutas acabadas

--- src/app/user/settings/settings-routing.module.ts
+++ src/app/user/settings/settings-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SettingsComponent } from './settings.component';
+import { ChangePassComponent } from './change-pass/change-pass.component';
+import { ExsmokerDataComponent } from './exsmoker-data/exsmoker-data.component';
+import { GeneralDataComponent } from './general-data/general-data.component';
 
-const routes: Routes = [{ path: '', component: SettingsComponent }];
+const routes: Routes = [
+  { path: '', component: SettingsComponent },
+  { path: 'changePass', component: ChangePassComponent },
+  { path: 'exSmokerData', component: ExsmokerDataComponent },
+  { path: 'generalData', component: GeneralDataComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
