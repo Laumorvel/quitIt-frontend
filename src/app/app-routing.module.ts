bbb624390ca055d64e0d75ad6b4ec1e592718a5f
lauramolez <lauramorelez@gmail.com>
2022-03-21 17:23:06 +0100

    terminado guardian

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardGuard } from './auth-guard.guard';
 
 const routes: Routes = [
   {
@@ -14,6 +15,7 @@ const routes: Routes = [
   },
   {
     path: 'administratorDashboard',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./administrator-dashboard/administrator-dashboard.module').then(
         (m) => m.AdministratorDashboardModule
@@ -33,31 +35,37 @@ const routes: Routes = [
   },
   {
     path: 'groupArea',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./group-area/group-area.module').then((m) => m.GroupAreaModule),
   },
   {
     path: 'userArea',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./user/user-area/user-area.module').then((m) => m.UserAreaModule),
   },
   {
     path: 'settings',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./user/settings/settings.module').then((m) => m.SettingsModule),
   },
   {
     path: 'friends',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./user/friends/friends.module').then((m) => m.FriendsModule),
   },
   {
     path: 'meetUps',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./user/meet-ups/meet-ups.module').then((m) => m.MeetUpsModule),
   },
   {
     path: 'achievements',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./user/achievements/achievements.module').then(
         (m) => m.AchievementsModule
@@ -65,6 +73,7 @@ const routes: Routes = [
   },
   {
     path: 'penalties',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./user/penalties/penalties.module').then(
         (m) => m.PenaltiesModule
@@ -72,6 +81,7 @@ const routes: Routes = [
   },
   {
     path: 'rankingCommunity',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./user/ranking-community/ranking-community.module').then(
         (m) => m.RankingCommunityModule
@@ -79,6 +89,7 @@ const routes: Routes = [
   },
   {
     path: 'commentsCommunity',
+    canActivate: [AuthGuardGuard],
     loadChildren: () =>
       import('./user/comments-community/comments-community.module').then(
         (m) => m.CommentsCommunityModule
