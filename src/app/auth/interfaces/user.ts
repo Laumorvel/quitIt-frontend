9079764915985cb7c72d078d6de0c4b5f132efbc
adelalira <aalira.96@gmail.com>
2022-03-21 14:28:57 +0100

    contact us y register casi acabado

---
+++ src/app/auth/interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number,
+    name:string,
+    lastname:string,
+    username: string,
+    email: string,
+    password: string,
+    moneyPerDay:number,
+    cigarettesBeforePerDay:number
+
+}
---
+++ src/app/auth/register/quiz/quiz.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz',
+  templateUrl: './quiz.component.html',
+  styleUrls: ['./quiz.component.css']
+})
+export class QuizComponent implements OnInit {
+
+  
+  @Input()nombre:string ="";
+  @Output()sendObjetivos = new EventEmitter<number[]>();
+  objetivos:number[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  cigarettesBeforePerDay!:number;
+  moneyPerDay!:number;
+
+  /**
+   * Consigue y envía por el decorador output los objetivos del usuario que se registra
+   */
+  enviaObjetivos(){
+    this.objetivos.push(this.cigarettesBeforePerDay, this.moneyPerDay);
+    this.sendObjetivos.emit(this.objetivos);
+  }
+
+}
