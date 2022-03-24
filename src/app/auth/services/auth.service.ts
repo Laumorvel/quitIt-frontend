7bb554e05abfbf3ed1a0c0f263118b18e9daeb52
adelalira <aalira.96@gmail.com>
2022-03-24 18:40:13 +0100

    arreglo userarea, comments community y creación formulario incidencias

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Message } from 'src/app/public/interfaces/message';
+import { Message } from 'src/app/public/interfaces/interfaces';
 import { environment } from 'src/environments/environment';
 import { AuthResponse } from '../interfaces/auth-response';
 import { User } from '../interfaces/user';
---
+++ src/app/public/interfaces/interfaces.ts
@@ -0,0 +1,43 @@
+export interface Message {
+    id: number,
+    fecha: string,
+    fromUser: string,
+    subject:string,
+    text: string,
+};
+
+export interface Commentario {
+  id:   number;
+  text: string;
+  user: User;
+  date: Date;
+};
+
+export interface User {
+  id:                       number;
+  name:                     string;
+  lastName:                 string;
+  email:                    string;
+  password:                 string;
+  rol:                      string;
+  daysInARowWithoutSmoking: number;
+  cigarettesAvoided:        number;
+  totalTimeWithoutSmoking:  number;
+  startDate:                Date;
+  cigarettesBeforePerDay:   number;
+  moneyPerDay:              number;
+  smokingDays:              number;
+  cigarettesSmoked:         number;
+  moneySaved:               number;
+  username:                 string;
+  timeWithoutSmoking:       number;
+  moneySmoker:              number;
+}
+
+export interface Incidence {
+  id:      number;
+  subject: string;
+  text:    null;
+  user:    User;
+  state:   string;
+}
