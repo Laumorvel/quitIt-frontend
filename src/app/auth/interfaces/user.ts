505f000f16fe1761b2535f4a84b4ee194303ff97
lauramolez <lauramorelez@gmail.com>
2022-03-28 21:15:58 +0200

    casi terminada la subida de imagen del usuario

--- src/app/auth/interfaces/user.ts
+++
@@ -1,8 +0,0 @@
-// To parse this data:
-//
-//   import { Convert, User } from "./file";
-//
-//   const user = Convert.toUser(json);
-//
-// These functions will throw an error if the JSON doesn't
-// match the expected interface, even if the JSON is valid.
--- src/app/user/services/file-service.service.ts
+++ src/app/user/services/file-service.service.ts
@@ -13,45 +13,56 @@ export class FileServiceService {
   constructor(private http: HttpClient) {}
 
   /**
-   * Introduce la imagen subida por el usuario en la base de datos
+   * Añade un nuevo fichero en caso de que el usuario no contenga ya uno
    * @param file
-   * @returns mensaje de que el archivo se ha subido correctamente
+   * @returns mensage de éxito o error al subir el fichero
    */
-  upload(file: File): Observable<HttpEvent<any>> {
+  addFile(file: File): Observable<HttpEvent<any>> {
     const formData: FormData = new FormData();
     formData.append('file', file);
-    const req = new HttpRequest('POST', `${this.urlBase}/upload`, formData, {
+    const req = new HttpRequest('POST', `${this.urlBase}/file`, formData, {
       reportProgress: true,
       responseType: 'json',
     });
-    //console.log(req)
     return this.http.request(req);
   }
 
-   //MÉTODO que hace una petición GET a /files y obtiene todos los ficheros almacenados en la base de datos
-   getFiles(): Observable<any> {
-    return this.http.get(`${this.urlBase}/files`);
+  /**
+   * Modifica la imagen que hay subida y la sustituye por otra que se pasa por parámetro
+   * @param file
+   * @param id
+   * @returns mensaje de error o éxito al subir el nuevo fichero
+   */
+  modifyFile(file: File, id: string): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
+    const req = new HttpRequest('PUT', `${this.urlBase}/file/${id}`, formData, {
+      reportProgress: true,
+      responseType: 'json',
+    });
+    return this.http.request(req);
   }
 
-  //MÉTODO que hace una petición DELETE a /files pasándole el fichero y lo elimina de la base de datos
-  deleteFile(file: File): Observable<HttpEvent<any>>{
-    const url = `${this.urlBase}/files/${file}`;
-    return this.http.delete<any>(url);
-  }
-
-  //MÉTODO que hace una petición GET a /files pasándole el nombre de un fichero y te devuelve el fichero que coincide con ese nombre
-  getFileByName(){
-    const name = localStorage.getItem('imgNAME')
-    const url = `${this.urlBase}/files/${name}`;
-
+  /**
+   * Consigue la imagen asociada al usuario
+   * @param id
+   * @returns fichero asociado al usuario que contiene su imagen
+   */
+  getFileByFileIdFromUser() {
+    const url = `${this.urlBase}/file`;
     return this.http.get<FileDB>(url);
   }
 
-  //MÉTODO que hace una petición GET a /file pasándole el id de un user y te devuelve el fichero que está asociado a ese user
-  getFileByUserID(id:number){
-
-    const url = `${this.urlBase}/file/${id}`
-    return this.http.get<FileDB>(url);
+  /**
+   * Transforma el fichero que le pasamos en un array de bits para poder visualizar la imagen
+   * @param file
+   * @returns traducción de la imagen en un array de bits
+   */
+  obtenerImagen(file: FileDB) {
+    const base64String = btoa(
+      String.fromCharCode(...new Uint8Array(file.data))
+    );
+    const source = `data:image/png;base64,${base64String}` + file.data;
+    return source;
   }
-
 }
